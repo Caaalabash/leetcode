68d371291e2e898ace2148093bc6defe08de0ead
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-08-07 21:34:27 +0800

    feat: 20

---
+++ 20.有效的括号/isValid.go
@@ -0,0 +1,24 @@
+package problem0020
+
+// 栈 + 遍历
+// 当为 ( [ { 时, 入栈
+// 当栈不为空且为 ) ] } 时, 和栈顶元素比较, 同则出栈
+func isValid(s string) bool {
+	var stack []string
+	m := map[string]string{
+		"(": ")",
+		"[": "]",
+		"{": "}",
+	}
+	for _, v := range s {
+		strV := string(v)
+		if strV == "{" || strV == "(" || strV == "[" {
+			stack = append(stack, strV)
+		} else if len(stack) != 0 && strV == m[stack[len(stack) - 1]] {
+			stack = stack[:len(stack) - 1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
