d9db3c5071aedf52ca2aafa7aaae38b1cd8e4511
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-08-09 10:33:04 +0800

    feat: 28

---
+++ 28.实现strStr/strStr.go
@@ -0,0 +1,18 @@
+package problem0028
+
+// 哭了, 全是简单题, 还要思考半天, 最后发现还是一个蠢办法
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+
+	l2 := len(needle)
+	for i := 0; i <= len(haystack) - l2; i++ {
+		if haystack[i: i + l2] == needle {
+			return i
+		}
+	}
+
+	return -1
+}
+
