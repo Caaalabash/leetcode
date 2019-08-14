a530ed0b9ce54bcb5e2b415e9db6a727c2771ec3
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-08-14 23:41:50 +0800

    feat: 11

---
+++ 11.盛最多水的容器/maxArea.go
@@ -0,0 +1,23 @@
+package problem0011
+
+// 双指针法
+// 20ms 5.9mb
+func maxAreaA(height []int) int {
+	var area int
+	left, right, temp := 0, len(height) - 1, 0
+
+	for left != right {
+		width := right - left
+		if height[left] >= height[right] {
+			temp = width * height[right]
+			right--
+		} else {
+			temp = width * height[left]
+			left++
+		}
+		if temp > area {
+			area = temp
+		}
+	}
+	return area
+}
