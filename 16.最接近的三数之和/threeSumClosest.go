4fd5ec0bfd44c8d7fb24cb4b63909ceb25af22f0
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-08-19 20:47:11 +0800

    feat: 16

---
+++ 16.最接近的三数之和/threeSumClosest.go
@@ -0,0 +1,35 @@
+package problem0016
+
+import (
+	"sort"
+)
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	ans := nums[0] + nums[1] + nums[2]
+	lens := len(nums)
+	for i := 0; i < lens; i++ {
+		left, right := i + 1, lens - 1
+		for left < right {
+			total := nums[i] + nums[left] + nums[right]
+			if abs(target - total) < abs(target - ans) {
+				ans = total
+			}
+			if total > target {
+				right--
+			} else if total < target {
+				left++
+			} else {
+				return ans
+			}
+		}
+	}
+	return ans
+}
+func abs(val int) int {
+	if val < 0 {
+		return -val
+	}
+	return val
+}
+
