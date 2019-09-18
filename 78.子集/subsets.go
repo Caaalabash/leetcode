e9cefbe2b293c0c8119db03ce3c7cad3ad212a98
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-09-18 22:36:03 +0800

    feat: 78

---
+++ 78.子集/subsets.go
@@ -0,0 +1,20 @@
+package problem0078
+
+func subsets(nums []int) [][]int {
+	var result [][]int
+
+	backTracking(nums, 0, &result, []int{})
+	return result
+}
+
+func backTracking(nums []int, index int, result *[][]int, temp []int)  {
+	*result = append(*result, temp)
+
+	for i := index; i < len(nums); i++ {
+		temp = append(temp, nums[i])
+		t := make([]int, len(temp))
+		copy(t, temp)
+		backTracking(nums, i+1, result, t)
+		temp = temp[:len(temp)-1]
+	}
+}
