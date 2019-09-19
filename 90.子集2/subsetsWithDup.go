dd2294dfe924192a15450a18db351575dca8186c
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-09-20 00:38:39 +0800

    feat: 90

---
+++ 90.子集2/subsetsWithDup.go
@@ -0,0 +1,25 @@
+package problem0090
+
+import "sort"
+
+func subsetsWithDup(nums []int) [][]int {
+	var result [][]int
+	sort.Ints(nums)
+	backTracking(nums, 0, &result, []int{})
+	return result
+}
+
+func backTracking(nums []int, index int, result *[][]int, temp []int)  {
+	*result = append(*result, temp)
+
+	for i := index; i < len(nums); i++ {
+		if i > index && nums[i] == nums[i-1] {
+			continue
+		}
+		temp = append(temp, nums[i])
+		t := make([]int, len(temp))
+		copy(t, temp)
+		backTracking(nums, i+1, result, t)
+		temp = temp[:len(temp)-1]
+	}
+}
