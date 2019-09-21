eb27cdf2aeb41d20112792a39fc763f559384da5
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-09-21 21:53:32 +0800

    feat: 136

---
+++ 136.只出现一次的数字/singleNumber.go
@@ -0,0 +1,15 @@
+package problem0136
+
+// 异或运算符的性质
+// 交换律 p ^ q = q ^ p
+// 结合律 p ^ (q ^ r) = (p ^ q) ^ r
+// 恒等律 p ^ 0 = p
+// 归零律 p ^ p = 0
+// 自反: p ^ q ^ q = p
+func singleNumber(nums []int) int {
+	var result int
+	for i, l := 0, len(nums); i < l; i++ {
+		result ^= nums[i]
+	}
+	return result
+}
