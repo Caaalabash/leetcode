c1b4b468745b89680161a6240da37442622a3c4a
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-09-25 00:10:15 +0800

    feat: 324

---
+++ 324.4的幂/isPowerOfFour.go
@@ -0,0 +1,36 @@
+package problem0324
+// 老实孩子的做法, 依旧是两点：
+// 1. 首先是2的幂
+// 2. 1出现在奇数位置上
+func isPowerOfFour(num int) bool {
+	// 先判断是否为2的幂
+	if num > 0 && num & (num - 1) == 0 {
+		// 如果num-1中1的个数为偶数, 则通过
+		if hammingWeight(num - 1) & 1 == 0 {
+			return true
+		}
+		return false
+	}
+	return false
+}
+
+// 位1的个数
+func hammingWeight(num int) int {
+	var count int
+	for i := 0; i < 32; i++ {
+		if  num & 1 == 1 {
+			count += 1
+		}
+		num >>= 1
+	}
+	return count
+}
+
+// 奇淫巧技
+// 0xaaaaaaaa = 10101010101010101010101010101010 (偶数位为1，奇数位为0）
+// 0x55555555 = 1010101010101010101010101010101 (偶数位为0，奇数位为1）
+// 那么要判断1是否出现在奇数位置上，只需要判断 num & 0xaaaaaaaa == 0即可
+// 最简答案
+func isPowerOfFour1(num int) bool {
+	return num > 0 && num & (num - 1) == 0 && num & 0xaaaaaaaa == 0
+}
