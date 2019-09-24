61697b8efbf98aee094bd9ba2fe52410d55dd8ba
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-09-24 16:43:39 +0800

    feat: 229

---
+++ 229.求众数2/majorityElement.go
@@ -0,0 +1,22 @@
+package problem0229
+
+func majorityElement(nums []int) []int {
+	var result []int
+	m := make(map[int]int)
+
+	for i, lens := 0, len(nums); i < lens; i++ {
+		count, ok := m[nums[i]]
+		if ok {
+			count++
+		} else {
+			count = 1
+		}
+		m[nums[i]] = count
+	}
+	for k, v := range m {
+		if v > len(nums) / 3 {
+			result = append(result, k)
+		}
+	}
+	return result
+}
