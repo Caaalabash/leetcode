f75860cb9f848ceb4cc71582989114cec46713ad
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-10-02 11:37:17 +0800

    feat: 162

---
+++ 162.寻找峰值/findPeakElement.go
@@ -0,0 +1,16 @@
+package problem0162
+
+// 只要数组中存在一个元素比相邻元素大，那么沿着它一定可以找到一个峰值
+func findPeakElement(nums []int) int {
+	left, right := 0, len(nums) - 1
+	for left < right {
+		mid := (left + right) >> 1
+		// 如果nums[mid] > nums[mid+1], 则左侧存在峰值
+		if nums[mid] > nums[mid+1] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
