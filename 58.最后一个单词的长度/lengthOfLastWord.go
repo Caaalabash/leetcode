5bb6819b967cfc241d5892fddf4a7b2871a8a23b
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-10-02 18:10:05 +0800

    feat: 58

---
+++ 58.最后一个单词的长度/lengthOfLastWord.go
@@ -0,0 +1,18 @@
+package problem0058
+
+import (
+	"strings"
+)
+
+func lengthOfLastWord(s string) int {
+	trimS := strings.Trim(s," ")
+
+	var result int
+	for i := len(trimS)-1; i >= 0; i-- {
+		if trimS[i] == ' ' {
+			break
+		}
+		result += 1
+	}
+	return result
+}
