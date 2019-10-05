923ae8ff2add09dcf6e4fdbddaef960c2275dcae
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-10-05 18:46:10 +0800

    feat: 704

---
+++ 704.二分查找/search.go
@@ -0,0 +1,17 @@
+package problem0704
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l < r {
+		mid := (l + r) >> 1
+		if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	if nums[l] != target {
+		return -1
+	}
+	return l
+}
