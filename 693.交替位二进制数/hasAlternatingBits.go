4c85175af1ef1f2b83ced615839b0eafcac502ea
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-10-06 22:26:53 +0800

    feat: 693

---
+++ 693.交替位二进制数/hasAlternatingBits.go
@@ -0,0 +1,14 @@
+package problem693
+
+func hasAlternatingBits(n int) bool {
+	prev := n & 1
+	n >>= 1
+	for n > 0 {
+		if n&1 == prev {
+			return false
+		}
+		prev = n & 1
+		n >>= 1
+	}
+	return true
+}
