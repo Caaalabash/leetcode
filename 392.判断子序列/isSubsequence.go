a59989537de6f8dc7f85fdb50f92fc8f65e185b2
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-10-07 21:15:20 +0800

    feat: 392

--- 392.判断子序列/isSubsequence.go
+++ 392.判断子序列/isSubsequence.go
@@ -1,2 +1,20 @@
 package problem0392
 
+// 给定字符串s和t，判断s是否为t的子序列
+// 双指针解法
+func isSubsequence(s string, t string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	j := 0
+	for i := 0; i < len(t); i++ {
+		if s[j] == t[i] {
+			j++
+			if j == len(s) {
+				return true
+			}
+		}
+
+	}
+	return false
+}
