9d6d0dcff2d4333ab01da5dcb125ab6d4170a16c
Calabash <calabash@CalabashdeMacBook-Pro.local>
2019-10-28 22:08:08 +0800

    feat: 100

---
+++ 100.相同的树/isSameTree.go
@@ -0,0 +1,17 @@
+package problem0100
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p != nil && q != nil && p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return false
+}
