2059a99ddeecb5f456ee178abc97a83b73f03821
Caaalabash <1121062986@qq.com>
2019-11-08 16:51:23 +0800

    feat: 645

---
+++ 645.错误的集合/findErrorNums.go
@@ -0,0 +1,19 @@
+package problem0645
+
+// 哈希表+数学: 等差数列求和 + 多余的数字 - nums列表 = 确实的数字
+func findErrorNums(nums []int) []int {
+	m := make(map[int]struct{})
+	total := len(nums) * (len(nums) + 1) / 2
+	result := []int{0, 0}
+
+	for _, v := range nums {
+		if _, ok := m[v]; ok {
+			result[0] = v
+		}
+		m[v] = struct{}{}
+		total -= v
+	}
+	result[1] = total + result[0]
+
+	return result
+}
