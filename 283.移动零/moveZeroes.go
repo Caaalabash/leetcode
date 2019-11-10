5aa8d760a0ea472d2a88e89c704c3e540503e704
Caaalabash <1121062986@qq.com>
2019-11-10 11:22:34 +0800

    feat: 283

---
+++ 283.移动零/moveZeroes.go
@@ -0,0 +1,12 @@
+package problem0283
+
+// 双指针，指针i正常遍历，指针j用于记录0值位置
+func moveZeroes(nums []int) {
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
+		}
+	}
+}
