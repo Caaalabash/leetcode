1626d7b87b47ddf7a17e2a1e4c5c306b4cc15184
Caaalabash <1121062986@qq.com>
2019-11-16 18:20:04 +0800

    feat: 112

---
+++ 112.路径总和/hasPathSum.go
@@ -0,0 +1,17 @@
+package problem0112
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Val == sum && root.Left == nil && root.Right == nil {
+		return true
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
