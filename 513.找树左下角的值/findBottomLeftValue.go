b15e8f94c497aa9a29e67a5f05353a91497af2cc
Caaalabash <1121062986@qq.com>
2019-11-20 15:07:44 +0800

    doc: 513

--- 513.找树左下角的值/findBottomLeftValue.go
+++ 513.找树左下角的值/findBottomLeftValue.go
@@ -6,6 +6,10 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 找树最左下角的值, 题意为: 如果最底层只有一个，那就返回最底层的点，如果最底层有多个，就返回最底层做左边的那个
+// 递归基于信任，每次向下"递"深度，当深度大于当前记录值时，更新结果以及记录值
+// 因为左侧的优先级要高一点，因此先递归左树
+// 如果你想先递归右树，current >= depth即可
 func findBottomLeftValue(root *TreeNode) int {
 	var result int
 	var depth int
@@ -15,11 +19,11 @@ func findBottomLeftValue(root *TreeNode) int {
 		if node == nil {
 			return
 		}
-		dfs(node.Left, currentDepth+1)
 		if currentDepth > depth {
 			depth = currentDepth
 			result = node.Val
 		}
+		dfs(node.Left, currentDepth+1)
 		dfs(node.Right, currentDepth+1)
 	}
 	dfs(root, 1)
