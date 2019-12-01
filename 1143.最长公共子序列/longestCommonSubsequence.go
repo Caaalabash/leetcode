1ecf811556503bd57b09b5291e2a3c4b13ce09eb
Caaalabash <1121062986@qq.com>
2019-12-01 14:09:35 +0800

    style: 1043

--- 1143.最长公共子序列/longestCommonSubsequence.go
+++ 1143.最长公共子序列/longestCommonSubsequence.go
@@ -1,10 +1,10 @@
 package problem1143
 
 // 给定两个字符串text1和text2，返回这两个字符串的最长公共子序列的长度
-// 对于【text1<x0,x2...,xn>】【text2<y0,y2...,ym>】两个母串，【sub<z0,...zk>】是其LCS
+// 对于【text1<...,xn>】【text2<...,ym>】两个母串，【sub<...,zk>】是其LCS
 // 如果xn = ym, 则有：xn = ym = zk, 有zk-1是xn-1与ym-1的LCS
-// 如果xn != ym, 则zk是xn与ym-1的LCS，或者是xn-1与ym的LCS
-// 那么用二维数组dp[i][j]记录串text1<x0,...xi-1>和串text2<y0,...yj-i>的LCS长度
+// 如果xn != ym, 则有：zk是xn与ym-1的LCS，或者是xn-1与ym的LCS
+// 那么用二维数组dp[i][j]记录串text1<...,xi-1>和串text2<...,yj-i>的LCS长度
 // 可以得到状态转移方程
 //           |0                            => i = 0 or j = 0
 // dp[i,j] = |dp[i-1, j-1] + 1             => text1<i> == text2<j>
