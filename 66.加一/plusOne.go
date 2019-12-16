00ed6224531d192b78bca9682a80faacdce23a6a
Caaalabash <1121062986@qq.com>
2019-12-16 18:03:12 +0800

    feat: 66

---
+++ 66.加一/plusOne.go
@@ -0,0 +1,13 @@
+package problem0066
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i] += 1
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	return append([]int{1}, digits...)
+}
