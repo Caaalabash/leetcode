3951bd3fdc3b57c507c8dd3525af94e1dbaba7f5
Caaalabash <1121062986@qq.com>
2019-12-29 15:32:59 +0800

    feat: 437

--- 437.路径总和3/pathSum.go
+++ 437.路径总和3/pathSum.go
@@ -1,1 +1,28 @@
-package _37_路径总和3
+package problem0437
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 双重递归
+func pathSum(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+
+	var dfs func(node *TreeNode, sum int) int
+	dfs = func(node *TreeNode, sum int) int {
+		if node == nil {
+			return 0
+		}
+		count := 0
+		if sum == node.Val {
+			count = 1
+		}
+		return dfs(node.Left, sum-node.Val) + dfs(node.Right, sum-node.Val) + count
+	}
+
+	return dfs(root, sum) + pathSum(root.Left, sum) + pathSum(root.Right, sum)
+}
