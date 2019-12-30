00b9366b47171e4d333aba5d38b69adf2cf01ef9
Caaalabash <1121062986@qq.com>
2019-12-30 21:08:50 +0800

    feat: 1302

---
+++ 1302.层数最深叶子节点的和/deepestLeavesSum.go
@@ -0,0 +1,31 @@
+package problem1302
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func deepestLeavesSum(root *TreeNode) int {
+	var (
+		result int
+		deep   int
+		dfs    func(node *TreeNode, depth int)
+	)
+	dfs = func(node *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+		if depth > deep {
+			result = 0
+			deep = depth
+		}
+		if depth == deep {
+			result += node.Val
+		}
+		dfs(node.Left, depth+1)
+		dfs(node.Right, depth+1)
+	}
+	dfs(root, 1)
+	return result
+}
