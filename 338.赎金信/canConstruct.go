c397b6a925ab4cfecfa9a1f3f1e140506fe7eb25
Caaalabash <1121062986@qq.com>
2020-01-19 22:35:11 +0800

    feat: 338

---
+++ 338.赎金信/canConstruct.go
@@ -0,0 +1,1 @@
+package problem0383// 判断第一个字符串能不能由第二个字符串里的字符构成func canConstruct(ransomNote string, magazine string) bool {	mSet := make([]int, 26)	for _, s := range magazine {		mSet[s-'a']++	}	for _, s := range ransomNote {		mSet[s-'a']--		if mSet[s-'a'] < 0 {			return false		}	}	return true}
