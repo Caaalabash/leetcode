be268b6f00321e1a71efb282fc88d2c7cab21ebd
Caaalabash <1121062986@qq.com>
2020-03-09 23:31:45 +0800

    refactor: 17

--- 17.电话号码的字母组合/letterCombinations.go
+++ 17.电话号码的字母组合/letterCombinations.go
@@ -1,6 +1,5 @@
 package problem0017
 
-// 键盘映射表
 var phoneMap = map[byte]string{
 	'2': "abc",
 	'3': "def",
@@ -13,27 +12,24 @@ var phoneMap = map[byte]string{
 }
 
 func letterCombinations(digits string) []string {
-	return dfs(digits, 0, "", []string{})
-}
-
-// Depth-First-Search
-// @param digits - 包含数字2-9的字符串
-// @param index - 索引
-// @param temp - 临时记录的序列字符串
-// @param result - 结果列表
-// 递归的终止条件: index索引值超过了字符串的长度
-// 递归返回给上一级的信息: 返回结果数组
-// 本级递归做的事情: 遍历对应按键中的字母, 递归！
-func dfs(digits string, index int, temp string, result []string) []string {
-	if index >= len(digits) {
-		if temp != "" {
-			result = append(result, temp)
+	var (
+		result    []string
+		backTrack func(path string, index int)
+	)
+	backTrack = func(path string, index int) {
+		if len(path) == len(digits) {
+			if path != "" {
+				result = append(result, path)
+			}
+			return
 		}
-		return result
+		options := phoneMap[digits[index]]
+		for i := 0; i < len(options); i++ {
+			path = path + string(options[i])
+			backTrack(path, index+1)
+			path = path[:len(path)-1]
+		}
 	}
-	option := phoneMap[digits[index]]
-	for i := 0; i < len(option); i++ {
-		result = dfs(digits, index+1, temp+string(option[i]), result)
-	}
+	backTrack("", 0)
 	return result
 }
