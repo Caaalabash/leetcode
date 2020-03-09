04d397c9a1030c852cda15ece672f271af59b20b
Caaalabash <1121062986@qq.com>
2020-03-09 23:00:41 +0800

    refactor: 40

--- 40.组合总和2/combinationSum2.go
+++ 40.组合总和2/combinationSum2.go
@@ -3,33 +3,33 @@ package problem0040
 import "sort"
 
 func combinationSum2(candidates []int, target int) [][]int {
-	var result [][]int
-	sort.Ints(candidates)
-
-	backTracking(&result, candidates, target, []int{}, 0)
-	return result
-}
-
-// 排序 [1, 1, 2, 5, 6, 7, 10] target = 8
-// 如果arr[i]与arr[i - 1]相同, continue
-func backTracking(result *[][]int, candidates []int, diff int, temp []int, index int) (goon bool) {
-	if diff == 0 {
-		t := make([]int, len(temp))
-		copy(t, temp)
-		*result = append(*result, t)
+	var (
+		result    [][]int
+		backTrack func(path []int, target int, index int) (shouldGoOn bool)
+	)
+	backTrack = func(path []int, target int, index int) (shouldGoOn bool) {
+		if target == 0 {
+			temp := make([]int, len(path))
+			copy(temp, path)
+			result = append(result, temp)
+			return true
+		}
+		if target < 0 {
+			return false
+		}
+		for i := index; i < len(candidates); i++ {
+			if i > index && candidates[i] == candidates[i-1] {
+				continue
+			}
+			path = append(path, candidates[i])
+			if !backTrack(path, target-candidates[i], i+1) {
+				break
+			}
+			path = path[:len(path)-1]
+		}
 		return true
-	} else if diff < 0 {
-		return false
 	}
-	for i, lens := index, len(candidates); i < lens; i++ {
-		if i > index && candidates[i] == candidates[i-1] {
-			continue
-		}
-		temp = append(temp, candidates[i])
-		if !backTracking(result, candidates, diff-candidates[i], temp, i+1) {
-			break
-		}
-		temp = temp[:len(temp)-1]
-	}
-	return true
+	sort.Ints(candidates)
+	backTrack([]int{}, target, 0)
+	return result
 }
