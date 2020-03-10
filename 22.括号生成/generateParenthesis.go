35544643a52e518c5f7a277ed78721d993ef05f8
Caaalabash <1121062986@qq.com>
2020-03-10 16:50:40 +0800

    refactor: 22

--- 22.括号生成/generateParenthesis.go
+++ 22.括号生成/generateParenthesis.go
@@ -1,22 +1,30 @@
 package problem0022
 
-// 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+// 回溯算法是一种算法是想，递归是一种算法结构，一般回溯法多用递归来实现
 func generateParenthesis(n int) []string {
-	res := make([]string, 0)
-	gene(&res, "", n, n)
-	return res
-}
-
-func gene(res *[]string, str string, left, right int) {
-	if left == 0 { // 此时表明left已经出栈完毕，需要把right中的全部出栈
-		for i := 0; i < right; i++ {
-			str += ")"
+	var (
+		result    []string
+		backTrack func(path string, left int, right int)
+	)
+	backTrack = func(path string, left int, right int) {
+		if left == 0 && right == 0 {
+			if path != "" {
+				result = append(result, path)
+			}
+			return
 		}
-		*res = append(*res, str)
-		return
+		// 如果左括号个数大于右括号个数，减枝，因为必定无效
+		if left > right {
+			return
+		}
+		if left > 0 {
+			backTrack(path+"(", left-1, right)
+		}
+		if right > 0 {
+			backTrack(path+")", left, right-1)
+		}
+
 	}
-	gene(res, str+"(", left-1, right)
-	if left < right {
-		gene(res, str+")", left, right-1)
-	}
+	backTrack("", n, n)
+	return result
 }
