254142892279cfea13882a3cbf0d561fe595a094
Caaalabash <1121062986@qq.com>
2020-03-26 20:42:51 +0800

    feat: 55

---
+++ 55.跳跃游戏/canJump.go
@@ -0,0 +1,14 @@
+package problem0055
+
+func canJump(nums []int) bool {
+	canReachIndex := 0
+	for i := 0; i < len(nums); i++ {
+		if i > canReachIndex {
+			return false
+		}
+		if i + nums[i] > canReachIndex {
+			canReachIndex = i + nums[i]
+		}
+	}
+	return true
+}
