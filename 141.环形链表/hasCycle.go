24df1883fe8adebb1fbd07f1fee9205165e3fca3
Caaalabash <1121062986@qq.com>
2020-04-08 18:26:32 +0800

    feat: 141

---
+++ 141.环形链表/hasCycle.go
@@ -0,0 +1,22 @@
+package problem0141
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 快慢指针判断链表是否有环
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow, fast := head, head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
