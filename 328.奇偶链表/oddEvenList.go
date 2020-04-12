93b67c34e88bdd8e51f4d3848a6e39392a9983e6
Caaalabash <1121062986@qq.com>
2020-04-12 21:35:37 +0800

    feat: 328

---
+++ 328.奇偶链表/oddEvenList.go
@@ -0,0 +1,23 @@
+package problem0328
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 想法：奇节点放在一个链表里，偶节点放在一个链表里，然后把偶链表接在奇链表的尾部
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	odd, even := head, head.Next
+	evenHead := even
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
