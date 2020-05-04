611e989f054bee29d3d840b3b6ed12b71b82950a
Caaalabash <1121062986@qq.com>
2020-05-04 17:08:39 +0800

    refactor: 322

--- 322.零钱兑换/coinChange.go
+++ 322.零钱兑换/coinChange.go
@@ -1,5 +1,7 @@
 package problem0322
 
+import "math"
+
 // 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
 // 如果没有任何一种硬币组合能组成总金额，返回 -1。
 // 输入：coins = [1, 2, 5], amount = 11
@@ -9,21 +11,30 @@ package problem0322
 // 探索dp[x]和dp[i-x]的关系：
 //   amount 0  1 2 3 4 5 6 7 8 9 10 11
 //   count  -1 1 1 2 2 1 2 2 3 4 2  3
-// 容易观察到dp[i] = dp[x] + dp[i-x], x属于coins且x<i
-// 因此有：dp[i] = min(dp[x] + dp[i-x]), x属于coins且x<i
+// 对于dp[10] = 2来说，当x属于coins时，有
+// dp[10] = dp[5] + dp[5]
+// dp[10] = dp[2] + dp[8]
+// dp[10] = dp[1] + dp[9]
+// 因此有：dp[i] = min(dp[i-x]) + 1(属于coins)
 func coinChange(coins []int, amount int) int {
 	dp := make([]int, amount+1)
 	for i := 1; i <= amount; i++ {
 		dp[i] = -1
+		minCount := math.MaxInt32
 		for _, x := range coins {
-			if x > i || dp[i-x] == -1 {
-				continue
+			if x <= i && dp[i-x] != -1 {
+				minCount = min(minCount, dp[i-x])
 			}
-			count := dp[i-x] + 1
-			if dp[i] == -1 || dp[i] > count {
-				dp[i] = count
-			}
+		}
+		if minCount != math.MaxInt32 {
+			dp[i] = minCount + 1
 		}
 	}
 	return dp[amount]
+}
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
