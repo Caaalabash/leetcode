daf162e6c290fb9d0ea44136719889c031bd268c
Caaalabash <1121062986@qq.com>
2020-06-15 14:07:16 +0800

    feat: 445

---
+++ 445.两数相加2/addTwoNumbers.go
@@ -0,0 +1,51 @@
+package problem0445
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type Stack struct {
+	slice []int
+}
+
+func (this *Stack) isEmpty() bool {
+	return len(this.slice) == 0
+}
+
+func (this *Stack) push(val int) {
+	this.slice = append(this.slice, val)
+}
+
+func (this *Stack) pop() int {
+	if this.isEmpty() {
+		return 0
+	}
+	result := this.slice[len(this.slice)-1]
+	this.slice = this.slice[:len(this.slice)-1]
+	return result
+}
+
+// 栈！
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	stack1, stack2 := &Stack{slice: make([]int, 0)}, &Stack{slice: make([]int, 0)}
+	for l1 != nil {
+		stack1.push(l1.Val)
+		l1 = l1.Next
+	}
+	for l2 != nil {
+		stack2.push(l2.Val)
+		l2 = l2.Next
+	}
+	var result *ListNode
+	carry := 0
+	for !stack1.isEmpty() || !stack2.isEmpty() || carry > 0 {
+		sum := carry + stack1.pop() + stack2.pop()
+		carry = sum / 10
+
+		node := &ListNode{Val: sum % 10}
+		node.Next = result
+		result = node
+	}
+	return result
+}
