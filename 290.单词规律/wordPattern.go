3a8583822a711b381e5f4cb15b219e2ec7dfa78d
Caaalabash <1121062986@qq.com>
2020-11-17 11:02:51 +0800

    feat: 290

---
+++ 290.单词规律/wordPattern.go
@@ -0,0 +1,29 @@
+package probelm0290
+
+import (
+	"strings"
+)
+
+func wordPattern(pattern string, s string) bool {
+	kvMap := make(map[string]string, 0)
+	vkMap := make(map[string]string, 0)
+	patternList := strings.Split(pattern, "")
+	wordList := strings.Split(s, " ")
+
+	if len(patternList) != len(wordList) {
+		return false
+	}
+	for index, p := range patternList {
+		if v1, ok1 := kvMap[p]; !ok1 {
+			kvMap[p] = wordList[index]
+		} else if v1 != wordList[index] {
+			return false
+		}
+		if v2, ok2 := vkMap[wordList[index]]; !ok2 {
+			vkMap[wordList[index]] = p
+		} else if v2 != p {
+			return false
+		}
+	}
+	return true
+}
