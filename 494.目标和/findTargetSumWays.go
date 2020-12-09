ad99bc50269fbd21ee754000552e2936b2956222
Caaalabash <1121062986@qq.com>
2020-12-09 17:06:51 +0800

    refactor: 494

--- 494.目标和/findTargetSumWays.go
+++ 494.目标和/findTargetSumWays.go
@@ -1,15 +1,16 @@
 package problem0494
 
-// 01背包
-// 将nums划分成2个子集A，B，分别代表分配 + 的数和分配 - 的数，存在如下关系：
-// sum(A) - sum(B) = S
-// ===> sum(A) = S + sum(B)
-// ===> sum(A) * 2 = S + sum(B) + sum(A)
-// ===> sum(A) = (S + sum(nums)) / 2
-// 原问题转换为：nums中存在几个子集A，使得A中元素和为(S + sum(nums)) / 2
-// 按照416解题
-// dp[0][...] = 0 ==> 没有物品的话，根本没办法装背包
-// dp[...][0] = 1 ==> 没有容量的话，什么都不装就是一种装法
+// 题目等价于将数组分成两个子集，使得 sum(A) = target + sum(B)
+// 两边同时加上sum(A)，得到 sum(A) = (target + sum(nums)) / 2
+// 回退到416题
+// 状态：「可选择的元素」、「元素和」
+// 选择：「选这个元素」、「不选这个元素」
+// dp[i][j] = x的含义：若只在前 i 个物品中选择，若当前背包的容量为 j，则最多有 x 种方法可以恰好装满背包。
+// 状态转移方程：dp[i][j] = 择优(dp[i-1][j], dp[i-1][j-nums[i]])
+// 择优操作：+
+// 边界条件：
+//   dp[...][0] = 1 ==> 什么都不装就是一种装法
+//   dp[0][...] = 0 ==> 没有物品就没有装法
 func findTargetSumWays(nums []int, S int) int {
 	sum := 0
 	for _, v := range nums {
@@ -20,9 +21,9 @@ func findTargetSumWays(nums []int, S int) int {
 	}
 	target := (S + sum) / 2
 	dp := [2001]int{1}
-	for i := 1; i <= len(nums); i++ {
-		for j := target; j >= nums[i-1]; j-- {
-			dp[j] += dp[j-nums[i-1]]
+	for i := 0; i < len(nums); i++ {
+		for j := target; j >= nums[i]; j-- {
+			dp[j] += dp[j-nums[i]]
 		}
 	}
 	return dp[target]
