5c52ecf26c23c67394120cb438eb1bb8be4a51d1
Caaalabash <1121062986@qq.com>
2021-04-28 11:20:07 +0800

    refactor: 424-js 使用未优化版本

--- 424.替换后的最长重复字符/characterReplacement.js
+++ 424.替换后的最长重复字符/characterReplacement.js
@@ -2,26 +2,32 @@
 // 给你一个仅由大写英文字母组成的字符串，你可以将任意位置上的字符替换成另外的字符，总共可最多替换 k 次。
 // 在执行上述操作后，找到包含重复字母的最长子串的长度。
 
-// 滑动窗口
-// 注意不要求连续
+// 滑动窗口，未优化的可读版本
 function characterReplacement(s, k) {
     const length = s.length
-    // 记录区间内各字符的出现次数
+    // 记录窗口中各字符的出现次数
     const cache = {}
-    // max = 区间内字符出现次数的最大值
     let max = 0,
         left = 0,
         right = 0
 
     while(right < length) {
         s[right] in cache ? cache[s[right]]++ : cache[s[right]] = 1
-        max = Math.max(max, cache[s[right]])
-        // if也可以
-        while (right - left + 1 - max > k) {
-            s[left] in cache && cache[s[left]]--
+
+        while (right - left + 1 - getMaxValue(cache) > k) {
+            cache[s[left]]--
             left++
         }
+        max = Math.max(max, right - left + 1)
         right++
     }
-    return right - left
+    return max
+}
+
+function getMaxValue(object) {
+    let val = 0
+    for (const n of Object.values(object)) {
+        val = Math.max(val, n)
+    }
+    return val
 }
