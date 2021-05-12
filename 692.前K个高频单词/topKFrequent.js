e69fad9a156b6e3761e6ea104f81ff71bf209ef5
Caaalabash <1121062986@qq.com>
2021-05-12 15:29:06 +0800

    feat: 692-js

---
+++ 692.前K个高频单词/topKFrequent.js
@@ -0,0 +1,75 @@
+function topKFrequent(words, k) {
+  const map = {}
+  for (const word of words) {
+    if (word in map) {
+      map[word]++
+    } else {
+      map[word] = 1
+    }
+  }
+  // 最大堆
+  const heap = new Heap(Object.entries(map), (a, b) => {
+    return a[1] === b[1] ? a[0] < b[0] : a[1] > b[1]
+  })
+  const result = []
+  for (let i = 0; i < k; i++) {
+    result.push(heap.pop()[0])
+  }
+  return result
+}
+
+class Heap {
+  constructor(data = [], less) {
+    this.data = data
+    this.less = less || ((a, b) => a < b)
+
+    if (this.length) {
+      for (let p = (this.length - 2) >> 1; p >= 0; p--) {
+        this._down(p)
+      }
+    }
+  }
+  get length() {
+    return this.data.length
+  }
+  peak() {
+    return this.data[0]
+  }
+  push(val) {
+    this.data.push(val)
+    this._up(this.length - 1)
+  }
+  pop() {
+    if (!this.length) {
+      return
+    }
+    this._swap(0, this.length - 1)
+    const popItem = this.data.pop()
+    this._down(0)
+    return popItem
+  }
+  _swap(i, j) {
+    [this.data[i], this.data[j]] = [this.data[j], this.data[i]]
+  }
+  _up(i) {
+    if (i <= 0) return
+    const pIndex = (i - 1) >> 1
+    if (this.less(this.data[i], this.data[pIndex])) {
+      this._swap(i, pIndex)
+      this._up(pIndex)
+    }
+  }
+  _down(i) {
+    let leftIndex = i * 2 + 1
+    if (leftIndex >= this.length) {
+      return
+    }
+    if (leftIndex + 1 < this.length && this.less(this.data[leftIndex+1], this.data[leftIndex])) {
+      leftIndex++
+    }
+    if (this.less(this.data[leftIndex], this.data[i])) {
+      this._swap(leftIndex, i)
+      this._down(leftIndex)
+    }
+  }
+}
