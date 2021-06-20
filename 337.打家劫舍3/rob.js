7544064e4319fb4032ca583adc3b66c28d2c0c97
longyao.li <longyao.li@dmall.com>
2021-06-20 17:44:18 +0800

    feat: 337-js

---
+++ 337.打家劫舍3/rob.js
@@ -0,0 +1,32 @@
+// 这次偷一颗二叉树，相邻节点被偷将触发报警
+// 进行树的层次遍历，并存储每一层的和，然后对每层的和进行动态规划求解 => 即一次偷一层
+// 对于如下的树，是可以偷3和4的
+//        2
+//       / \
+//      1   3
+//     /
+//    4
+
+// f(node)表示选择node节点的情况下，node节点的子树上被选择的节点的最大权重值
+// g(node)表示不选择node节点的情况下，node节点的子树上被选择的节点的最大权重值
+// 选中node时，node.left、node.right一定不能选
+// => f(node) = node.val + g(node.left) + g(node.right)
+// 不选中node时，可以选中node.left(right)，也可以不选中node.left(right)
+// => g(node) = Max(f(node.left), g(node.left)) + Max(f(node.right), g(node.right))
+// 太猛了这
+function rob(root) {
+    const f = new Map()
+    const g = new Map()
+    // 采取后序遍历
+    const dfs = node => {
+        if (node === null) {
+            return
+        }
+        dfs(node.left)
+        dfs(node.right)
+        f.set(node, node.val + (g.get(node.left) || 0) + (g.get(node.right) || 0))
+        g.set(node, Math.max(f.get(node.left) || 0, g.get(node.left) || 0) + Math.max(f.get(node.right) || 0, g.get(node.right) || 0))
+    }
+    dfs(root)
+    return Math.max(f.get(root) || 0, g.get(root) || 0)
+}
