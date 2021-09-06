ebb0406b0301d651fb29e2ece297cb2c8724a966
Caaalabash <caaalabash@gmail.com>
2021-09-06 18:51:10 +0800

    refactor: 494-js

--- 494.目标和/findTargetSumWays.js
+++ 494.目标和/findTargetSumWays.js
@@ -1,42 +1,36 @@
-// 数组元素和为 sum，添加减号的元素和为neg，则添加加号的元素和为sum-neg
-// 可得 (sum-neg) - neg = target ===> neg = (sum - target) / 2
-// 本题转换为：在数组 nums 中选取若干元素，使得这些元素之和等于 neg 的方案数
-function findTargetSumWays(nums, target) {
-  const sum = nums.reduce((acc, i) => (acc += i), 0)
-  if (sum - target < 0 || (sum - target) % 2 === 1) return 0
-  const realTarget = (sum - target) / 2
-  // 初始化dp数组n+1的原因：在考虑初始值的时候，没有方案也是一种方案
-  // dp[i][j] = 选取前i个数，其和为j的方案数量，需要求dp[n][realTarget]
-  const n = nums.length
-  const dp = Array.from({ length: n + 1 },
-    () => Array.from({ length: realTarget + 1 },
-      () => 0)
-  )
-  // 如果没有任何元素可选，和为0，方案数为1
-  dp[0][0] = 1
-  for (let i = 1; i <= n; i++) {
-    for (let j = 0; j <= realTarget; j++) {
-      dp[i][j] = dp[i-1][j]
-      if (j >= nums[i-1]) {
-        dp[i][j] += dp[i-1][j-nums[i-1]]
-      }
-    }
-  }
-  return dp[n][realTarget]
-}
+// 设：所有添加 - 号的和为 a，所有添加 + 号的和为 b，数组和为sum，数组最大元素为maxNum
+// 有：(sum - a) - a = target
+// 有：a = (sum - target) / 2
+// 问题转换为在 nums 中寻找目标和为 (sum - target) / 2 的数目
 
-function findTargetSumWays1(nums, target) {
-  const sum = nums.reduce((acc, i) => (acc += i), 0)
-  if (sum - target < 0 || (sum - target) % 2 === 1) return 0
+// 简单的过滤条件
+// 由于0 <= nums[i] <= 1000
+// 1. target > sum，return 0
+// 2. sum - target为奇数，return 0
+
+// 然后进入 0-1 背包问题的套路
+// dp定义：dp[i][j] => 在[0, i]区间中，和为j的方式数
+// 初始值：dp[0][0] => 当没有任何元素可以选取时，元素和只能是 0，对应的方案数是 1
+// 因此，dp数组应该初始化为 (nums.length+1) * (realTarget+1) 求dp[nums.length][realTarget]
+
+// 常见的nums[i]变成了nums[i-1]，因为dp数组初始化多了一个
+// dp[i][j] = dp[i-1][j-nums[i-1]] (j >= nums[i-1])
+// dp[i][j] = dp[i-1][j] (j < nums[i-1])
+function findTargetSumWays(nums, target) {
+  const sum = nums.reduce((acc, i) => acc += i, 0)
+  if (target > sum) {
+    return 0
+  }
   const realTarget = (sum - target) / 2
-  // 初始化dp数组n+1的原因：在考虑初始值的时候，没有方案也是一种方案
-  // dp[i][j] = 选取前i个数，其和为j的方案数量，需要求dp[n][realTarget]
-  const n = nums.length
-  const dp = Array.from({ length: realTarget + 1 }, () => 0)
+  if (realTarget % 1 !== 0) {
+    return 0
+  }
+  // 直接使用优化空间的dp求解
+  const dp = new Array(realTarget+1).fill(0)
   dp[0] = 1
-  for (let i = 1; i <= n; i++) {
+  for (let i = 1; i <= nums.length; i++) {
     for (let j = realTarget; j >= nums[i-1]; j--) {
-      dp[j] += dp[j-nums[i-1]]
+      dp[j] += dp[j - nums[i-1]]
     }
   }
   return dp[realTarget]
