d83d3bc1d38ab2c7574c8859e787d5e01049b5c6
Caaalabash <caaalabash@gmail.com>
2021-09-06 19:40:33 +0800

    refactor: 416-js

--- 416.分割等和子集/canPartition.js
+++ 416.分割等和子集/canPartition.js
@@ -1,73 +1,43 @@
+// 设：数组和为sum，数组最大元素为maxNum
+
+// 简单的过滤
+// 0. nums.length < 2，return false
+// 1. sum为奇数，return false
+// 2. maxNum > sum/2，return false
+
+// 然后进入 0-1 背包套路
+// dp[i][j]: 前[0, i)个元素和为 j，能否分割成两个子集，使得两个子集的元素和相等
+// 初始值：
+//   dp[0][nums[0]] = true    选第一个，和为第一个 = true
+//   dp[i][0] = true          不选，和为0        = true
+// 状态转移方程
+//   dp[i][j] = dp[i-1][j] (j < nums[i])
+//   dp[i][j] = dp[i-1][j-nums[i]] (j > nums[i])
+// dp数组初始化为 (n) * (target + 1)，求dp[n-1][target]
 function canPartition(nums) {
-  // 排除的一些条件
-  // 1. nums.length < 2
-  // 2. sum(nums)为奇数
-  // 3. max(nums) > sum/2
   const n = nums.length
   if (n < 2) {
     return false
   }
   let sum = 0
   let maxNum = 0
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i]
-    maxNum = nums[i] > maxNum ? nums[i] : maxNum
-  }
-  if (sum % 2 === 1) {
-    return false
-  }
-  const target = sum / 2
-  if (maxNum > target) {
-    return false
-  }
-  // 动态规划的背包问题
-  // dp[i][j]：从数组的 [0, i] 这个子区间内挑选一些正整数，使得这些数的和恰好等于 j => 求出 dp[n-1][target]
-  const dp = Array.from({ length: n }, () => Array.from({ length: target + 1 }, () => false))
-  // 初始值
-  // dp[i][0] = true
-  // dp[0][nums[0]] = true
-  for (let i = 0; i < dp.length; i++) {
-    dp[i][0] = true
-  }
-  dp[0][nums[0]] = true
-  // 比较nums[i]（当前要选择的物品）与j（背包容量）的关系
-  // 如果j < nums[i] => dp[i][j] = dp[i-1][j]
-  // 如果j >= nums[i] => dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
-  for (let i = 1; i < n; i++) {
-    for (let j = 1; j <= target; j++) {
-      if (j < nums[i]) {
-        dp[i][j] = dp[i - 1][j]
-      } else {
-        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]]
-      }
-    }
-  }
-  return dp[n-1][target]
-}
-
-// 空间压缩
-function canPartition1(nums) {
-  const n = nums.length
-  if (n < 2) {
-    return false
-  }
-  let sum = 0, maxNum = 0
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i]
-    maxNum = nums[i] > maxNum ? nums[i] : maxNum
-  }
-  if (sum % 2 === 1) {
-    return false
-  }
-  const target = sum / 2
-  if (maxNum > target) {
-    return false
-  }
-  const dp = Array.from({ length: target + 1 }).fill(false)
-  dp[0] = true
   for (let i = 0; i < n; i++) {
+    sum += nums[i]
+    maxNum = nums[i] > maxNum ? nums[i] : maxNum
+  }
+  const target = sum / 2
+  if (target % 1 !== 0) {
+    return 0
+  }
+  if (maxNum > target) {
+    return 0
+  }
+  // 直接使用优化空间的dp求解
+  const dp = new Array(target + 1).fill(false)
+  dp[0] = true
+  for (let i = 1; i < n; i++) {
     for (let j = target; j >= nums[i]; j--) {
-      dp[j] = dp[j] || dp[j - nums[i]]
+      dp[j] |= dp[j - nums[i]]
     }
   }
   return dp[target]
