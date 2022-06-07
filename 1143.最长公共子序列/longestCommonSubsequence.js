fd40d5f4074184e976801d9f1ee21ad0e9bed0f1
Caaalabash <caaalabash@gmail.com>
2022-06-08 00:55:45 +0800

    refactor: code

--- 1143.最长公共子序列/longestCommonSubsequence.js
+++ 1143.最长公共子序列/longestCommonSubsequence.js
@@ -1,16 +1,13 @@
-// 寻找2个字符串中的最长公共子序列
-// 设dp[i][j]为text1[0:i)，text2[0:j)的最长公共子序的长度
-// 为什么右侧是开区间？
-// 因为需要考虑到i = 0 or j = 0的边界情况
-
+// LCS
+// 状态定义：dp[i][j] 表示 text1[0:i) 和 text2[0:j) 的 LCS
+// 边界情况：dp[0][j] = dp[i][0] = 0
+// 时间复杂度：O(mn)
+// 空间复杂度：O(mn)
 function longestCommonSubsequence(text1, text2) {
     const l1 = text1.length
     const l2 = text2.length
 
-    const dp = new Array(l1 + 1)
-    for (let i = 0; i < l1 + 1; i++) {
-        dp[i] = new Array(l2 + 1).fill(0)
-    }
+    const dp = new Array(l1 + 1).fill(0).map(() => new Array(l2 + 1).fill(0));
 
     for (let i = 1; i <= l1; i++) {
         for (let j = 1; j <= l2; j++) {
--- 300.最长上升子序列/lengthOfLIS.js
+++ 300.最长上升子序列/lengthOfLIS.js
@@ -1,22 +1,26 @@
-// dp[i]: 以nums[i]结尾的最长严格递增子序列的长度
-// 详细的可以参考673题
+// LIS
+// 通常会想：dp[i][j] 代表 nums[i:j] 的 LIS，但是这样推导不出来
+// 正确定义：dp[i] 代表以 nums[i] 结尾的 LIS
+// 显然有初始值: dp[i] = 1
+// 状态转移方程：dp[i] = max(dp[j]) + 1 (0 <= j < i && nums[j] < nums[i])
+
+// 时间复杂度 O(n^2)
+// 空间复杂度 O(n)
 function lengthOfLIS(nums) {
     const l = nums.length
     const dp = new Array(l).fill(1)
-    let maxLen = 0
+    let result = 0
 
     for (let i = 0; i < l; i++) {
+        let max = 0
         for (let j = 0; j < i; j++) {
             if (nums[j] < nums[i]) {
-                if (dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j]
-                }
+                max = Math.max(max, dp[j])
             }
         }
-        if (dp[i] > maxLen) {
-            maxLen = dp[i]
-        }
+        dp[i] = max + 1
+        result = Math.max(result, dp[i])
     }
 
-    return maxLen
+    return result
 }
