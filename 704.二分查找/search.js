07a39a4cc0565c954cb437816c5bd09dfe51ca03
Caaalabash <caaalabash@gmail.com>
2022-06-10 01:25:27 +0800

    feat: 704-js

---
+++ 704.二分查找/search.js
@@ -0,0 +1,18 @@
+function search(nums, target) {
+	let left = 0
+	let right = nums.length - 1
+
+	while (left < right) {
+		const mid = (left + right) >> 1
+		if (nums[mid] === target) {
+			return mid
+		}
+		if (nums[mid] > target) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return nums[left] === target ? left : -1
+}
