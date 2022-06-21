bb5bf204b071c8a1bbfd1de0310add6b8ea1354c
Caaalabash <caaalabash@gmail.com>
2022-06-21 22:46:53 +0800

    feat: 416-js

--- 416.分割等和子集/canPartition.js
+++ 416.分割等和子集/canPartition.js
@@ -1,44 +1,41 @@
-// 设：数组和为sum，数组最大元素为maxNum
+// 01背包问题
+// 设dp[i][j]为：前[0, i]个元素和为j，能否求出【题目问题】
+// 显然有
+//   dp[i][j] = dp[i - 1][j] (不取i)
+//   dp[i][j] = dp[i - 1][j - nums[i]] (取i)
+// 初始值有
+//   dp[0][nums[0]] = true (取第一个，和为nums[0])
+//   dp[i][0] = true (不取，和为0)
 
-// 简单的过滤
-// 0. nums.length < 2，return false
-// 1. sum为奇数，return false
-// 2. maxNum > sum/2，return false
-
-// 然后进入 0-1 背包套路
-// dp[i][j]: 前[0, i)个元素和为 j，能否分割成两个子集，使得两个子集的元素和相等
-// 初始值：
-//   dp[0][nums[0]] = true    选第一个，和为第一个 = true
-//   dp[i][0] = true          不选，和为0        = true
-// 状态转移方程
-//   dp[i][j] = dp[i-1][j] (j < nums[i])
-//   dp[i][j] = dp[i-1][j-nums[i]] (j > nums[i])
-// dp数组初始化为 (n) * (target + 1)，求dp[n-1][target]
+// 直接写01背包的优化方程
+// dp[j] = dp[j] || dp[j - nums[i]]
+// dp[0] = true
 function canPartition(nums) {
-  const n = nums.length
-  if (n < 2) {
-    return false
-  }
-  let sum = 0
-  let maxNum = 0
-  for (let i = 0; i < n; i++) {
-    sum += nums[i]
-    maxNum = nums[i] > maxNum ? nums[i] : maxNum
-  }
-  const target = sum / 2
-  if (target % 1 !== 0) {
-    return 0
-  }
-  if (maxNum > target) {
-    return 0
-  }
-  // 直接使用优化空间的dp求解
-  const dp = new Array(target + 1).fill(false)
-  dp[0] = true
-  for (let i = 1; i < n; i++) {
-    for (let j = target; j >= nums[i]; j--) {
-      dp[j] |= dp[j - nums[i]]
+    // 排除数组长度不够、和为奇数、最大值超过数组一半的情况
+    const n = nums.length
+    if (n <= 1) {
+        return false
     }
-  }
-  return dp[target]
+    let sum = 0
+    let max = 0
+    for (let i = 0; i < n; i++) {
+        sum += nums[i]
+        max = Math.max(max, nums[i])
+    }
+    if (sum % 2 !== 0) {
+        return false
+    }
+    const target = sum / 2
+    if (max > target) {
+        return false
+    }
+    // dp优化方程
+    const dp = new Array(target + 1).fill(0)
+    dp[0] = true
+    for (let i = 1; i < n; i++) {
+        for (let j = target; j >= nums[i]; j--) {
+            dp[j] |= dp[j - nums[i]]
+        }
+    }
+    return dp[target]
 }
