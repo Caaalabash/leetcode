d379f9a90fae21f8d36ffc0c412b9cc2c14cc70a
Caaalabash <caaalabash@gmail.com>
2022-06-21 23:23:15 +0800

    feat: 494-js

--- 416.分割等和子集/canPartition.js
+++ 416.分割等和子集/canPartition.js
@@ -1,14 +1,10 @@
 // 01背包问题
-// 设dp[i][j]为：前[0, i]个元素和为j，能否求出【题目问题】
+// 设dp[i][j]为：前[0, i)个元素和为j，能否求出【题目问题】
 // 显然有
 //   dp[i][j] = dp[i - 1][j] (不取i)
-//   dp[i][j] = dp[i - 1][j - nums[i]] (取i)
-// 初始值有
-//   dp[0][nums[0]] = true (取第一个，和为nums[0])
-//   dp[i][0] = true (不取，和为0)
-
+//   dp[i][j] = dp[i - 1][j - nums[i - 1]] (取i)
 // 直接写01背包的优化方程
-// dp[j] = dp[j] || dp[j - nums[i]]
+// dp[j] = dp[j] || dp[j - nums[i - 1]]
 // dp[0] = true
 function canPartition(nums) {
     // 排除数组长度不够、和为奇数、最大值超过数组一半的情况
@@ -32,9 +28,9 @@ function canPartition(nums) {
     // dp优化方程
     const dp = new Array(target + 1).fill(0)
     dp[0] = true
-    for (let i = 1; i < n; i++) {
-        for (let j = target; j >= nums[i]; j--) {
-            dp[j] |= dp[j - nums[i]]
+    for (let i = 1; i <= n; i++) {
+        for (let j = target; j >= nums[i - 1]; j--) {
+            dp[j] |= dp[j - nums[i - 1]]
         }
     }
     return dp[target]
--- 494.目标和/findTargetSumWays.js
+++ 494.目标和/findTargetSumWays.js
@@ -1,22 +1,9 @@
-// 设：所有添加 - 号的和为 a，所有添加 + 号的和为 b，数组和为sum，数组最大元素为maxNum
-// 有：(sum - a) - a = target
-// 有：a = (sum - target) / 2
-// 问题转换为在 nums 中寻找目标和为 (sum - target) / 2 的数目
-
-// 简单的过滤条件
-// 由于0 <= nums[i] <= 1000
-// 1. target > sum，return 0
-// 2. sum - target为奇数，return 0
-
-// 然后进入 0-1 背包问题的套路
-// dp定义：dp[i][j] => 在[0, i]区间中，和为j的方式数
-// 初始值：dp[0][0] => 当没有任何元素可以选取时，元素和只能是 0，对应的方案数是 1
-// 因此，dp数组应该初始化为 (nums.length+1) * (realTarget+1) 求dp[nums.length][realTarget]
-
-// 常见的nums[i]变成了nums[i-1]，因为dp数组初始化多了一个
-// dp[i][j] = dp[i-1][j-nums[i-1]] (j >= nums[i-1])
-// dp[i][j] = dp[i-1][j] (j < nums[i-1])
+// 由于 (sum - 作为正数的数字和) - 作为正数的数字和 = target
+// 所以目标和为 target 转换为目标和为 (sum - target) / 2
+// 每个元素的状态也从"添加加号"、"添加减号" 变成了 "选"、"不选"
+// 问题转换为01背包问题
 function findTargetSumWays(nums, target) {
+  // 排除基本情况
   const sum = nums.reduce((acc, i) => acc += i, 0)
   if (target > sum) {
     return 0
@@ -26,11 +13,11 @@ function findTargetSumWays(nums, target) {
     return 0
   }
   // 直接使用优化空间的dp求解
-  const dp = new Array(realTarget+1).fill(0)
+  const dp = new Array(realTarget + 1).fill(0)
   dp[0] = 1
   for (let i = 1; i <= nums.length; i++) {
-    for (let j = realTarget; j >= nums[i-1]; j--) {
-      dp[j] += dp[j - nums[i-1]]
+    for (let j = realTarget; j >= nums[i - 1]; j--) {
+      dp[j] += dp[j - nums[i - 1]]
     }
   }
   return dp[realTarget]
