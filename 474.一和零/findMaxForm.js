7620be1bf9a43fdae550c435083135c97f7a1310
Caaalabash <caaalabash@gmail.com>
2022-06-21 23:45:33 +0800

    feat: 474-js

--- 474.一和零/findMaxForm.js
+++ 474.一和零/findMaxForm.js
@@ -1,29 +1,36 @@
-// 最多有m个0和n个1 => 视为0,1背包问题
-// dp[i][j] = 使用 i 个 0 和 j 个 1的最大子集大小
-// 很容易推出： dp[i][j] = Max(dp[i - strs[i]的0的数量][j - strs[i]的1的数量] + 1, dp[i][j])
-function findMaxForm(strs, m, n) {
-  const dp = new Array(m + 1)
-  for (let i = 0; i < m + 1; i++) {
-    dp[i] = new Array(n + 1).fill(0)
-  }
+// 经典的背包问题可以使用二维动态规划求解，两个维度分别是物品和容量。
+// 这道题有两种容量，因此需要使用三维动态规划求解，三个维度分别是字符串、0 的容量和 1 的容量。
+// 定义三位数组 dp[k][i][j]：在前 k 个字符串中，使用 i 个 0 和 j 个 1的情况下最多可以得到的字符串数量
+// 最终的答案为 dp[strs.length][m][n]
 
-  for (const str of strs) {
-    const { zero, one } = getMeta(str)
-    for (let i = m; i >= zero; i--) {
-      for (let j = n; j >= one; j--) {
-        dp[i][j] = Math.max(dp[i][j], dp[i - zero][j - one] + 1)
-      }
+// 如果不能选第 k 个字符串：
+//   dp[k][i][j] = dp[k - 1][j][k]
+// 如果能选第 k 个字符串：
+//   dp[k][i][j] = Max(dp[k - 1][i][j], dp[k - 1][i - 0的数量][j - 1的数量] + 1)
+function findMaxForm(strs, m, n) {
+    const dp = new Array(m + 1)
+    for (let i = 0; i <= m; i++) {
+        dp[i] = new Array(n + 1).fill(0)
     }
-  }
-  return dp[m][n]
+
+    for (const str of strs) {
+        const {zero, one} = getMeta(str)
+        for (let i = m; i >= zero; i--) {
+            for (let j = n; j >= one; j--) {
+                dp[i][j] = Math.max(dp[i][j], dp[i - zero][j - one] + 1)
+            }
+        }
+    }
+
+    return dp[m][n]
 }
 
 function getMeta(str) {
-  let zero = 0
-  let one = 0
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === '0') zero++
-    else one++
-  }
-  return { zero, one }
+    let zero = 0
+    let one = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '0') zero++
+        else one++
+    }
+    return {zero, one}
 }
