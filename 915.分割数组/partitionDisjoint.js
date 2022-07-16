e1cffd939c11378c8e0ebb4eec00b9307a151cd5
Caaalabash <caaalabash@gmail.com>
2022-07-16 21:26:59 +0800

    feat: 915-js

--- 915.分割数组/partitionDisjoint.js
+++ 915.分割数组/partitionDisjoint.js
@@ -14,3 +14,27 @@ function partitionDisjoint(nums) {
     }
     return pos + 1
 }
+
+// 正向遍历，维护max[i], 为nums[0:i] 的最大值
+// 逆向遍历，维护min[i], 为nums[i:] 的最小值
+// 比较max、min数组，如果 max[i] < min[i+1], 答案找到
+function partitionDisjoint(nums) {
+    const length = nums.length
+    const max = new Array(length)
+    const min = new Array(length)
+    max[0] = nums[0]
+    min[length - 1] = nums[length - 1]
+
+    for (let i = 1; i < length; i++) {
+        max[i] = Math.max(max[i - 1], nums[i])
+    }
+    for (let i = length - 2; i >= 0; i--) {
+        min[i] = Math.min(min[i + 1], nums[i])
+    }
+    for (let i = 0; i < length - 1; i++) {
+        if (max[i] <= min[i + 1]) {
+            return i + 1
+        }
+    }
+    return -1
+}
