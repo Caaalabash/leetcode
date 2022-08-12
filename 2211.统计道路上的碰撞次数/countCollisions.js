9f6e228232efcaf50bb179fe79e60be84dba6400
Caaalabash <caaalabash@gmail.com>
2022-08-12 17:40:41 +0800

    feat: 2211-js

--- 2211.统计道路上的碰撞次数/countCollisions.js
+++ 2211.统计道路上的碰撞次数/countCollisions.js
@@ -1,40 +1,37 @@
 // Q2 1581
 // 栈模拟
 function countCollisions(directions) {
-    const length = directions.length
-    let stack = [directions.charAt(0)]
+    let stack = [directions[0]]
     let result = 0
+    let cur, last
 
-    for (let i = 1; i < length; i++) {
-        let cur = directions.charAt(i)
-        let last = stack[stack.length - 1]
+    for (let i = 1; i < directions.length; i++) {
+        cur = directions[i]
+        last = stack[stack.length - 1]
 
         if (last === cur) {
-            if (cur !== 'S') {
-                stack.push(cur)
-            }
+            stack.push(cur)
             continue
         }
         if (last === 'R') {
             // RRRL -> RRRS
             if (cur === 'L') {
-                result += 1
+                result++
                 cur = 'S'
             }
             // RRRS -> S
             while (last === 'R') {
-                result += 1
+                result++
                 stack.pop()
                 last = stack[stack.length - 1]
             }
-            stack = ['S']
+            stack.push('S')
         } else if (last === 'S' && cur === 'L') {
-            result += 1
-            stack = ['S']
+            result++
+            stack.push('S')
         } else {
-            stack = [cur]
+            stack.push(cur)
         }
     }
-
     return result
 }
