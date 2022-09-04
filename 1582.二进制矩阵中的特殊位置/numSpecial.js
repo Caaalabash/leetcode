e9450f05936bb468e60fc33609d8e9621a8b2411
Caaalabash <caaalabash@gmail.com>
2022-09-04 17:08:22 +0800

    feat: 1582-js daily

--- 1582.二进制矩阵中的特殊位置/numSpecial.js
+++ 1582.二进制矩阵中的特殊位置/numSpecial.js
@@ -30,3 +30,25 @@ function numSpecial(mat) {
 
     return result
 }
+
+// 补充
+var numSpecial = function(mat) {
+    const m = mat.length, n = mat[0].length;
+    const rowsSum = new Array(m).fill(0);
+    const colsSum = new Array(n).fill(0);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            rowsSum[i] += mat[i][j];
+            colsSum[j] += mat[i][j];
+        }
+    }
+    let res = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 1 && rowsSum[i] === 1 && colsSum[j] === 1) {
+                res++;
+            }
+        }
+    }
+    return res;
+};
