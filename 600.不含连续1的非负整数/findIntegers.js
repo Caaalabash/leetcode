05db6ba70fd559e7dd70b91baf11dd8285b22b21
Caaalabash <caaalabash@gmail.com>
2022-10-20 11:43:12 +0800

    feat: 600

---
+++ 600.不含连续1的非负整数/findIntegers.js
@@ -0,0 +1,29 @@
+// 数位DP
+function findIntegers(n) {
+    const str = Number(n).toString(2)
+    const len = str.length
+    // TODO: 解释一下这里为什么不需要前导零标识参数
+    const dfs = memo((recur, i, status, isLimit) => {
+        if (i === len) {
+            return 1
+        }
+        let result = 0
+        const end = isLimit ? Number(str[i]) : 1
+        for (let k = 0; k <= end; k++) {
+            if (status === 1 && k === 1) continue
+            result += recur(i + 1, k, isLimit && k === end)
+        }
+        return result
+    })
+    return dfs(0, 0, true)
+}
+function memo(fn) {
+    const cache = new Map()
+    return function recur(...args) {
+        const key = args.toString()
+        if (cache.has(key)) {
+            return cache.get(key)
+        }
+        return cache.set(key, fn(recur, ...args)).get(key)
+    }
+}
