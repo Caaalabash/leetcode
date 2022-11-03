4a129393d9698463bc5875be20627f12bd9aa2f5
Caaalabash <caaalabash@gmail.com>
2022-11-03 15:02:35 +0800

    feat: 300

--- 300.最长上升子序列/lengthOfLIS.js
+++ 300.最长上升子序列/lengthOfLIS.js
@@ -24,3 +24,73 @@ function lengthOfLIS(nums) {
 
     return result
 }
+
+
+// 二分
+// 时间复杂度O(nlogn)
+// 空间复杂度O(n)
+function lengthOfLIS(nums) {
+    const n = nums.length
+    if (n <= 1) {
+        return n
+    }
+    const d = [nums[0]]
+
+    for (let i = 1; i < n; i++) {
+        if (nums[i] > d[d.length - 1]) {
+            d.push(nums[i])
+            continue
+        }
+        let l = 0
+        let r = d.length - 1
+        while (l < r) {
+            const mid = (l + r) >> 1
+            if (d[mid] < nums[i]) {
+                l = mid + 1
+            } else {
+                r = mid
+            }
+        }
+        d[l] = nums[i]
+    }
+    return d.length
+}
+
+// 求LIS的一个解
+function oneOfLIS(nums) {
+    const n = nums.length
+    if (n <= 1) {
+        return nums
+    }
+    const prev = nums.slice()
+    const result = [0] // 记录索引
+
+    for (let i = 1; i < n; i++) {
+        if (nums[result[result.length - 1]] < nums[i]) {
+            prev[i] = result[result.length - 1]
+            result.push(i)
+            continue
+        }
+        let left = 0
+        let right = result.length - 1
+        while (left < right) {
+            const mid = (left + right) >> 1
+            if (nums[result[mid]] < nums[i]) {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+        if (left > 0) {
+            prev[i] = result[left - 1]
+        }
+        result[left] = i
+    }
+    let len = result.length
+    let val = result[len - 1]
+    while (len-- > 0) {
+        result[len] = val
+        val = prev[val]
+    }
+    return result
+}
